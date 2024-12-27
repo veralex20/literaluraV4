29deb6a23e333e3ae61012a300c5f35dae26f2f2
Alexis <153469896+veralex20@users.noreply.github.com>
2024-12-27 12:50:35 -0300

    Initial commit

---
+++ literaluraV4/src/main/java/com/example/literaluraV4/LiteraluraV4Application.java
@@ -0,0 +1,13 @@
+package com.example.literaluraV4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraV4Application.class, args);
+	}
+
+}
---
+++ literaluraV4/src/test/java/com/example/literaluraV4/LiteraluraV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.literaluraV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
